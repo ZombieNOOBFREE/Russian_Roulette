da5e92f1f35c896ff71432c9ad0744909a9af62a
ZombieNOOBFREE <ruds2210@gmail.com>
2024-02-10 17:26:25 +0500

    shot, empty, spin

---
+++ app/src/main/java/ru/example/russianroulette/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.example.russianroulette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.jar.Attributes;
+
+public class MainActivity extends Activity {
+
+
+    private SoundPool sounds;
+    private int sound_shot;
+    private int sound_misfire;
+    private int sound_drum;
+    private ImageView blood_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        sounds = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                        .build();
+        loadSounds();
+        blood_image = findViewById(R.id.imageView2);
+    }
+
+    private void loadSounds(){
+        sound_shot = sounds.load(this, R.raw.revolver_shot, 1);
+        sound_misfire = sounds.load(this, R.raw.gun_false, 1);
+        sound_drum = sounds.load(this, R.raw.revolver_baraban, 1);
+
+    }
+
+    public void onShot(View view) {
+        sounds.play(sound_shot, 1.0f, 1.0f, 1, 0, 1 );
+        blood_image.setVisibility(View.VISIBLE);
+    }
+
+    public void onMisFire(View view) {
+        sounds.play(sound_misfire, 1.0f, 1.0f, 1, 0, 1 );
+        blood_image.setVisibility(View.INVISIBLE);
+    }
+
+    public void onDrum(View view) {
+        sounds.play(sound_drum, 1.0f, 1.0f, 1, 0, 1 );
+        blood_image.setVisibility(View.INVISIBLE);
+    }
+}
